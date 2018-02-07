c2f583b1e47af954407566c4f546fd787095676b
Kachel Wittig <kjwittig@gmail.com>
2018-02-07 18:12:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {}
 
 function updatePlaylist(playlist, artist, song) {
-  return Object.assign({}, playlist, {[artist]: value} )
+  return Object.assign({}, playlist, {[artist]: song} )
 }
 
 function removeFromPlaylist() {}
